f37992cc61b080bfb9635763da37bdcbc7b55658
Thomas Skardal <thomas.skardal@gmail.com>
2013-09-08 18:42:19 +0200

    This is currently a mess, but at least it's a start

---
+++ init.el
@@ -0,0 +1,50 @@
+(require 'package)
+
+;; Add melpa to package repos
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
+
+(load-theme 'tango-dark)
+(setq-default indent-tabs-mode nil)
+
+;; language specifics
+(eval-after-load 'js2-mode '(require 'setup-js2-mode))
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; end 
+
+(require 'magit)
+
+;;(add-to-list 'load-path
+;;              "~/.emacs.d/elpa/yasnippet-0.8.0")
+
+(require 'yasnippet)
+(yas/global-mode 1)
+
+;; Completion that uses many different methods to find options.
+(global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
+(global-set-key (kbd "C-:") 'hippie-expand-lines)
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
---
+++ setup-js2-mode.el
@@ -0,0 +1,17 @@
+(font-lock-add-keywords
+ 'js2-mode `(("\\<\\(function\\) *("
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "\u0192") nil)))))
+
+;; js2-mode steals TAB, let's steal it back for yasnippet
+(defun js2-tab-properly ()
+  (interactive)
+  (let ((yas-fallback-behavior 'return-nil))
+    (unless (yas-expand)
+      (indent-for-tab-command)
+      (if (looking-back "^\s*")
+          (back-to-indentation)))))
+
+(define-key js2-mode-map (kbd "TAB") 'js2-tab-properly)
+
+(provide 'setup-js2-mode)
