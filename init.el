b79362534706bca7df16a3037f4acebd7af0700c
Thomas Skardal <thomas.skardal@gmail.com>
2014-12-15 08:55:07 +0100

    Default dir is /Users/thomas.skardal. Some key mapping for MacOS

--- init.el
+++ init.el
@@ -9,3 +9,15 @@
 
 ;; A fresh start
 (require 'better-defaults)
+
+;; Disable splash screen
+(setq inhibit-startup-message t)
+
+;; MacOS specific key bindings
+(when (eq system-type 'darwin)
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char)) ;; sets fn-delete to be right-delete
+
+;; Use home dir as default dir
+(setq default-directory (concat (getenv "HOME") "/"))
