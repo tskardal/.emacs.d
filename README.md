aa5bb76f41905047be4f908b4b46e9a055547575
Thomas Skardal <thomas.skardal@gmail.com>
2015-04-28 19:48:55 +0200

    wipe

--- README.md
+++
@@ -1,4 +0,0 @@
-.emacs.d
-========
-
-My Emacs setup. Using https://github.com/technomancy/better-defaults.
--- custom/themes.el
+++
@@ -1,3 +0,0 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;(add-to-list 'load-path "~/.emacs.d/themes")
-(load-theme 'afternoon t)
